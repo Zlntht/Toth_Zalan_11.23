538f10b579a7751c3eea8281598a3f29d541d996
Tóth Zalán <toth.zalan@students.jedlik.eu>
2022-11-23 10:11:56 +0100

    Function 2, Main

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-from data import objektivek
+from data import nev, gyujtotavolsag, ar
 from os import system
 
 filename='objektivek.csv'
 
 def menu():
     system('cls')
-    print('-----------------MENÜ-------------------')
+    print('--------------------MENÜ----------------------')
     print('0 - Kilépés')
     print('1 - Összes objektív a raktárban')
     print('2 - Objektív keresése ár alapján')
@@ -13,7 +13,19 @@ def menu():
     print('4 - Objektív eladása')
     return input('Kérem válasszon: ')
 
-def loadObjektivek():
-    file=open(filename,'r',encoding='utf-8')
-
+def fajlBeolvasas():
+    file=open('objektivek.csv','r', encoding='utf-8')
+    file.readline() 
+    for egysor in file:
+        darabolt=egysor.strip().split(';')
+        nev.append(darabolt[0])
+        gyujtotavolsag.append(float(darabolt[1]))
+        ar.append(float(darabolt[2]))
     file.close()
+
+def loadObjektivek():
+    system('cls')
+    print('-------------OBJEKTIVEK RAKTÁRON--------------\n') 
+    for i in range(0,len(nev)):
+        print(f'\t{nev[i]} {gyujtotavolsag[i]}mm \n\t\tÁr: {ar[i]} Ft\n ')
+    input('\nTovább...')
---
+++ main.py
@@ -0,0 +1,15 @@
+from functions import *
+from os import system
+
+fajlBeolvasas()
+valasztas=''
+while valasztas!='0':
+    valasztas=menu()
+    if valasztas=='1':
+        loadObjektivek()
+    elif valasztas=='2':
+        pass
+    elif valasztas=='3':
+        pass
+    elif valasztas=='4':
+        pass
